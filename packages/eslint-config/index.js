58ddf9fc2c221f4c0ee358b0c0a7624b47d40d08
John Kelvie <jpk@bespoken.io>
2021-09-18 10:42:21 -0400

    Updating rules for eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,7 +6,8 @@ module.exports = {
     "ecmaVersion": 2020
   },
   "plugins": [
-    "jsdoc"
+    "jsdoc",
+    "sort-class-members"
   ],
   "rules": {
     "jsdoc/no-undefined-types": [2,
@@ -25,10 +26,35 @@ module.exports = {
       }
     ],
     "jsdoc/require-param": 2,
+    "jsdoc/require-param-type": 2,
+    "jsdoc/require-property-type": 2,
     "jsdoc/require-returns": [2, {
       "forceRequireReturn": true
     }],
     "jsdoc/require-returns-type": 2,
-    "no-unused-vars": 2
+    "no-unused-vars": 2,
+    "sort-class-members/sort-class-members": [
+			2,
+			{
+				"order": [
+					"[static-properties]",
+					"[static-methods]",
+					"constructor",
+					"[properties]",
+					"[conventional-private-properties]",
+					"[methods]",
+					"[conventional-private-methods]"
+				],
+				"accessorPairPositioning": "getThenSet"
+			}
+		],
+    "sort-keys": ["error", "asc", {"caseSensitive": false, "natural": false, "minKeys": 2}],
+    "sort-imports": ["error", {
+      "ignoreCase": false,
+      "ignoreDeclarationSort": true,
+      "ignoreMemberSort": false,
+      "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+      "allowSeparatedGroups": false
+  }]
   }
 }
