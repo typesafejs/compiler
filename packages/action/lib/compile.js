298202e053a3b6a56cbea0210b8d1ec1cc919204
John Kelvie <jpk@bespoken.io>
2021-09-22 02:27:48 -0400

    Updated compile action

--- packages/action/lib/compile.js
+++ packages/action/lib/compile.js
@@ -1,4 +1,4 @@
-const Process = require('@typesafejs/shared/lib/process')
+const Process = require('./process')
 
 /**
  * Runs the main routine
@@ -6,6 +6,8 @@ const Process = require('@typesafejs/shared/lib/process')
  */
 async function main() {
   console.info('cwd: ' + process.cwd())
+  await Process.spawn('npx yarn install')
+  await Process.spawn('npx lerna bootstrap')
   await Process.spawn('npx lerna exec -- tsc --project jsconfig.json')
 }
 
