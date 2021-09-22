7c74d26f58f8d2205860110b9125f8058fb54416
John Kelvie <jpk@bespoken.io>
2021-09-22 02:36:07 -0400

    Pulling in dependencies

--- packages/action/lib/compile.js
+++ packages/action/lib/compile.js
@@ -1,4 +1,3 @@
-const core = require('@actions/core')
 const Process = require('./process')
 
 /**
@@ -7,6 +6,9 @@ const Process = require('./process')
  */
 async function main() {
   console.info('cwd: ' + process.cwd())
+  await Process.spawn('npx yarn add @action/core')
+  const core = require('@actions/core')
+
   await Process.spawn('npx yarn install')
   await Process.spawn('npx lerna bootstrap')
   const result = await Process.spawn('npx lerna exec -- tsc --project jsconfig.json')
