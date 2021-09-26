99ee232190ba4a09fb5cc0daf18030d51e27ab01
John Kelvie <jpk@bespoken.io>
2021-09-26 13:23:08 -0400

    More debugging to compile script

--- packages/action/lib/compile.js
+++ packages/action/lib/compile.js
@@ -7,6 +7,7 @@ const Process = require('./process')
 async function main() {
   console.info('now 2 cwd: ' + process.cwd())
   await Process.spawn('npx yarn add -W @actions/core')
+  console.info("Done with install of actions/core")
   await Process.spawn('npx yarn install')
   await Process.spawn('npx lerna bootstrap')
   const result = await Process.spawn('npx lerna exec -- tsc --project jsconfig.json')
