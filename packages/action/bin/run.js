1916a10279bc80d1e4bc2c7ce610b08a746e645b
John Kelvie <jpk@bespoken.io>
2021-09-26 18:26:36 -0400

    Lots of updates to sdk

--- packages/action/bin/run.js
+++
@@ -1,8 +0,0 @@
-const Action = require('../lib/action')
-
-console.info('ACTION RUN: ' + JSON.stringify(process.argv))
-const action = new Action()
-  
-if (process.argv[2] === 'compile') {
-  action.compile()
-}
